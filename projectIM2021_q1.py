5228e002f2c5930d993e640cc3d1c0915a113d14
royiluz <royi.iluz@gmail.com>
2021-02-20 15:15:54 +0200

    Q1 - Load images from folder and show them.

--- projectIM2021_q1.py
+++ projectIM2021_q1.py
@@ -1,3 +1,22 @@
+import cv2
+import os
+import matplotlib.pyplot as plt
+
+
+# Load all images from folder by folder's name
+def load_images_from_folder(folder):
+    images_ = []
+    for filename in os.listdir(folder):
+        img_ = cv2.imread(os.path.join(folder, filename), cv2.IMREAD_GRAYSCALE)
+        if img_ is not None:
+            images_.append(img_)
+    return images_
+
 
 if __name__ == '__main__':
-    print('Hello world - 1')
+    # load all images:
+    images = load_images_from_folder('images')
+    for img in images:
+        plt.imshow(img, cmap='gray')
+        plt.axis('off')
+        plt.show()
